eee7cd303e893ab1ace78964d32bcab4071cbf5c
Conor Kingston <cmkingston21@me.com>
2018-12-12 09:18:27 -0500

    previews are generated with relative path

--- shape-models/objects.js
+++ shape-models/objects.js
@@ -5,29 +5,29 @@ $(document).ready(function() {
     // search tool
     $('#query').on('keyup', function(event) {
         event.stopPropagation();
-        
+
         const str = this.value.toUpperCase();
         const regex = new RegExp(str,'g');
         bodies = Bodies();
-        
+
         if (!str) {
             makeTable();
             return;
         };
-        
+
         let keys = Object.keys(Bodies());
         let filteredNewBodies = keys.filter(body => {
             body = body.toUpperCase();
             const test = regex.test(body);
             return test;
         });
-        
+
         keys.map(key => {
             if (filteredNewBodies.indexOf(key) === -1) {
                 delete bodies[key];
             };
         });
-        
+
         makeTable();
     });
 });
@@ -45,9 +45,9 @@ function makeTable() {
             <div class="cell">Preview</div>
         </div>
     </div>`;
-    
+
     shapeModelTable.replaceWith(table);
-    
+
     const reAlpha = /[^a-zA-Z]/g;
     const reNum = /[^0-9]/g;
 
@@ -62,41 +62,41 @@ function makeTable() {
             return aA > bA ? 1 : -1;
         }
     }
-    
+
     objectNames.sort(sortAlphaNum).map(name => {
         const $table = $("#shape-model-table");
-        
+
         const $row = $('<div>', {class: 'row'});
-        
+
         const newCell = function(element) {
             const $cell = $('<div>', {class: 'cell'});
             $cell.append(element);
             return $cell;
         };
-        
+
         // construct name cell
         const $name = newCell(`<p><b>${name}</b></p>`);
         $row.append($name);
-        
+
         // construct link to dataset cell
         const $datasetDiv = $('<div>',{class: 'cell'});
         const $datasetList = $('<ul>');
-        
+
         $datasetDiv.append($datasetList);
-        
+
         const datasets = bodies[name]['datasets'];
         datasets.map(dataset => {
             $datasetList.append(`<li><a href="${dataset.link}" target="_blank">${dataset.name}</a></li>`);
         });
         $row.append($datasetDiv);
-        
+
         // construct data ferret search link cell
         function querify(qs) {
             return qs.replace(/ /g,'%20');
         };
         const ferretLink = `https://sbnapps.psi.edu/ferret/SimpleSearch/results.action?targetName=${querify(name)}`;
         $row.append(newCell(`<a href="${ferretLink}" target="_blank">Ferret Search</a>`));
-        
+
         // construct files cell
         const $filesCell = $('<div>', {class: 'cell'});
         const $list = $('<ul>');
@@ -105,15 +105,15 @@ function makeTable() {
         titles.map(title => {
             $list.append(`<li><a href="${files[title]}" target="_blank" download>${title}</a></li>`)
         });
-        
+
         $filesCell.append($list);
         $row.append($filesCell);
-        
+
         // construct preview cell
         const preview = bodies[name]['preview'];
         if (!preview) $row.append(newCell(''));
-        else $row.append(newCell(`<a href="${'http://localhost:8000/pds/shape-models/' + preview}" target="_blank"><img src="${preview}" class="preview" title="Click to Enlarge"></a>`));
-        
+        else $row.append(newCell(`<a href="${preview}" target="_blank"><img src="${preview}" class="preview" title="Click to Enlarge"></a>`));
+
         $table.append($row);
     });
 };
