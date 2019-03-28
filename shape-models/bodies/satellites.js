c220e68ccc6bfd621ff1a506462b25c03a0e542a
Conor Kingston <cmkingston21@me.com>
2019-03-28 15:33:28 -0700

    thebe

--- shape-models/bodies/satellites.js
+++ shape-models/bodies/satellites.js
@@ -156,17 +156,13 @@ let Satellites = function() {
                 }
             }]
         },
+        'Thebe (Jupiter XIV)': {
+            datasets: [stookDataset(nullset)]
+        },
 
 
 
         
-        // 'Thebe (Jupiter XIV)': {
-        //     files: {
-        //         'Archived Shape File': 'https://sbnarchive.psi.edu/pds3/non_mission/EAR_A_5_DDR_STOOKE_SHAPE_MODELS_V2_0/data/'
-        //     },
-        //     datasets: [stookDataset],
-        //     preview: null
-        // },
         // 'Larissa (Neptune VII)': {
         //     files: {
         //         'Archived Shape File': 'https://sbnarchive.psi.edu/pds3/non_mission/EAR_A_5_DDR_STOOKE_SHAPE_MODELS_V2_0/data/'
