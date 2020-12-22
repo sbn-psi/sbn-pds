5d105a3256ec1075b074964a4ec2bdb29b482d0a
mdrum <mdrum@psi.edu>
2020-12-21 17:47:05 -0700

    fix umd links

--- shape-models/js/app.Data.js
+++ shape-models/js/app.Data.js
@@ -4,11 +4,11 @@ app.factory('Comets', function () {
                 type: 'comet',
                 datasets: [{
                     name: 'Rosetta Shape Models of Comet 67P/C-G',
-                    link: 'https://pds-sbn.astro.umd.edu/holdings/ro-c-multi-5-67p-shape-v2.0/dataset.html',
+                    link: 'https://pdssbn.astro.umd.edu/holdings/ro-c-multi-5-67p-shape-v2.0/dataset.html',
                     files: {
                         data: {
                             primary: {
-                                downloadLink: 'https://pds-sbn.astro.umd.edu/holdings/ro-c-multi-5-67p-shape-v2.0/data/triplate/mspcd_lam/shap2/cg_mspcd_shap2_001m_cart.wrl',
+                                downloadLink: 'https://pdssbn.astro.umd.edu/holdings/ro-c-multi-5-67p-shape-v2.0/data/triplate/mspcd_lam/shap2/cg_mspcd_shap2_001m_cart.wrl',
                                 fileFormat: 'WRL'
                             },
                             derived: [{
@@ -33,11 +33,11 @@ app.factory('Comets', function () {
                 type: 'comet',
                 datasets: [{
                     name: 'Satdust NAVCAM 81P/Wild 2 Shape Model',
-                    link: 'https://pds-sbn.astro.umd.edu/holdings/sdu-c-navcam-5-wild2-shape-model-v2.1/dataset.html',
+                    link: 'https://pdssbn.astro.umd.edu/holdings/sdu-c-navcam-5-wild2-shape-model-v2.1/dataset.html',
                     files: {
                         data: {
                             primary: {
-                                downloadLink: 'https://pds-sbn.astro.umd.edu/holdings/sdu-c-navcam-5-wild2-shape-model-v2.1/data/wild2_cart_full.tab',
+                                downloadLink: 'https://pdssbn.astro.umd.edu/holdings/sdu-c-navcam-5-wild2-shape-model-v2.1/data/wild2_cart_full.tab',
                                 fileFormat: 'TAB'
                             },
                             derived: [{
@@ -62,11 +62,11 @@ app.factory('Comets', function () {
                 type: 'comet',
                 datasets: [{
                     name: 'Shape Model of Hartley 2',
-                    link: 'https://pds-sbn.astro.umd.edu/holdings/dif-c-hriv_mri-5-hartley2-shape-v1.0/',
+                    link: 'https://pdssbn.astro.umd.edu/holdings/dif-c-hriv_mri-5-hartley2-shape-v1.0/',
                     files: {
                         data: {
                             primary: {
-                                downloadLink: 'https://pds-sbn.astro.umd.edu/holdings/dif-c-hriv_mri-5-hartley2-shape-v1.0/data/hartley2_2012_cart.wrl',
+                                downloadLink: 'https://pdssbn.astro.umd.edu/holdings/dif-c-hriv_mri-5-hartley2-shape-v1.0/data/hartley2_2012_cart.wrl',
                                 fileFormat: 'WRL'
                             },
                             derived: [{
@@ -91,11 +91,11 @@ app.factory('Comets', function () {
                 type: 'comet',
                 datasets: [{
                     name: 'Deep Impact/Stardust-NExT Derived Shape Model',
-                    link: 'https://pds-sbn.astro.umd.edu/holdings/dif-c-hriv_its_mri-5-tempel1-shape-v2.0/dataset.html',
+                    link: 'https://pdssbn.astro.umd.edu/holdings/dif-c-hriv_its_mri-5-tempel1-shape-v2.0/dataset.html',
                     files: {
                         data: {
                             primary: {
-                                downloadLink: 'https://pds-sbn.astro.umd.edu/holdings/dif-c-hriv_its_mri-5-tempel1-shape-v2.0/data/tempel1_2012_cart.wrl',
+                                downloadLink: 'https://pdssbn.astro.umd.edu/holdings/dif-c-hriv_its_mri-5-tempel1-shape-v2.0/data/tempel1_2012_cart.wrl',
                                 fileFormat: 'WRL'
                             },
                             derived: [{
@@ -440,11 +440,11 @@ app.factory('Comets', function () {
                 type: 'asteroid',
                 datasets: [{
                     name: 'Rosetta Shape Model of Asteroid Lutetia',
-                    link: 'http://pds-sbn.astro.umd.edu/holdings/ro-a-osinac_osiwac-5-lutetia-shape-v1.0/dataset.html',
+                    link: 'http://pdssbn.astro.umd.edu/holdings/ro-a-osinac_osiwac-5-lutetia-shape-v1.0/dataset.html',
                     files: {
                         data: {
                             primary: {
-                                downloadLink: 'https://pds-sbn.astro.umd.edu/holdings/ro-a-osinac_osiwac-5-lutetia-shape-v1.0/data/lutetia_003m_cart.wrl',
+                                downloadLink: 'https://pdssbn.astro.umd.edu/holdings/ro-a-osinac_osiwac-5-lutetia-shape-v1.0/data/lutetia_003m_cart.wrl',
                                 fileFormat: 'WRL'
                             },
                             derived: [{
@@ -618,11 +618,11 @@ app.factory('Comets', function () {
                 type: 'asteroid',
                 datasets: [{
                     name: 'Rosetta Shape Model of Asteroid Steins',
-                    link: 'https://pds-sbn.astro.umd.edu/holdings/ro-a-osinac_osiwac-5-steins-shape-v1.0/dataset.html',
+                    link: 'https://pdssbn.astro.umd.edu/holdings/ro-a-osinac_osiwac-5-steins-shape-v1.0/dataset.html',
                     files: {
                         data: {
                             primary: {
-                                downloadLink: 'https://pds-sbn.astro.umd.edu/holdings/ro-a-osinac_osiwac-5-steins-shape-v1.0/data/steins_cart.wrl',
+                                downloadLink: 'https://pdssbn.astro.umd.edu/holdings/ro-a-osinac_osiwac-5-steins-shape-v1.0/data/steins_cart.wrl',
                                 fileFormat: 'WRL'
                             },
                             derived: [{
