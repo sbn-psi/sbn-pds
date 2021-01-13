fe6b039841e17d6305f27c98f9febd6e3022de87
mdrum <mdrum@psi.edu>
2021-01-13 11:46:07 -0700

    change appearance of search bar, and make it reactive to focus events

--- js/search.js
+++ js/search.js
@@ -1,14 +1,35 @@
-// 'use strict';
+'use strict';
 var searchApp = angular.module('search', []);
 
 searchApp.directive('searchApp', function() {
     return {
         templateUrl: '/pds-staging/search-app.html',
-        controller: function($scope, searchData) {
+        controller: function($scope, searchData, $window) {
             $scope.view = {
                 searchData,
                 focused: false,
                 query: ''
+            }
+
+            $scope.keyup = function(event) {
+                switch (event.keyCode) {
+                    case 27: // Escape key
+                        event.target.blur(); break;
+                    case 13: // Enter key
+                        if($scope.filteredResults.length === 1) {
+                            $window.open($scope.filteredResults[0].url, '_self')
+                        }
+                }
+            }
+
+            $scope.inputFocused = function() {
+                $scope.view.focused = true;
+            }
+            $scope.loseFocus = function(event) {
+                let container = event.target.closest('.search-container')
+                if(!container.contains(event.relatedTarget)) {
+                    $scope.view.focused = false;
+                }
             }
         }
     }
