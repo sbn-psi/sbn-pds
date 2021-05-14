a2ef4627280d3ab0f795d89302460acaf5c440e3
mdrum <mdrum@psi.edu>
2021-05-14 11:05:11 -0700

    more pds-staging redirects

--- js/search-data.js
+++ js/search-data.js
@@ -2,142 +2,142 @@ searchData.splice(0, 0, {
         name: 'Altimetry',
         subtitle: '',
         description: ' These data sets contain altimetry data which is collected by measuring reflected laser light from the surface of an object. This allows for measurements of height, distance, and altitude. Altimeters can utilize radio waves as well as lasers to measure distance.  ',
-        url: '/pds/archive/altimetry.html',
+        url: '/pds-staging/archive/altimetry.html',
         category: 'Data Types'
     },{
         name: 'Plasma/Particles/Magnetosphere',
         subtitle: '',
         description: 'These data sets contain data collected by a magnometer. A Magnometer is used to collect data about the magnetic forces of an object. These observation sets can contain magnetic field data and well as detected electromagnetic anomalies.',
-        url: '/pds/archive/plasma.html',
+        url: '/pds-staging/archive/plasma.html',
         category: 'Data Types'
     },{
         name: 'Image Data',
         subtitle: '',
         description: ' These data sets contain Photographic image data, which can be either from ground based telescopes or from a spacecraft. These data sets will contain numerous images of an object or a group of objects. These data sets are mostly asteroid data sets with some comets and other solar system bodies. ',
-        url: '/pds/archive/image.html',
+        url: '/pds-staging/archive/image.html',
         category: 'Data Types'
     },{
         name: 'Maps and Mosaics',
         subtitle: '',
         description: 'These data sets contain Mosaic images which are a single large image stitched together from numerous smaller images to give a wide field view of an area.  Usually, it is constructed from a series of spacecraft images taken closely together in time so that the Sun angle is consistent throughout the mosaic; however, for global mosaics, that is not possible.  Additionally, the images are frequently photometrically corrected so that the reflected light is normalized to a single emission angle, typically 30°.',
-        url: '/pds/archive/maps.html',
+        url: '/pds-staging/archive/maps.html',
         category: 'Data Types'
     },{
         name: 'NEO Surveys',
         subtitle: '',
         description: ' These data sets contain Astronomical Survey observations. An astronomical survey is a generalized search over all (or most) of the sky and lacks a specific target. For SBN, its goal is to discover comets and asteroids and improve our knowledge of their orbits.  Alternatively, an astronomical survey may comprise a set of many images or spectra of objects that share a common type or feature. ',
-        url: '/pds/archive/surveys.html',
+        url: '/pds-staging/archive/surveys.html',
         category: 'Data Types'
     },{
         name: 'Nuclear',
         subtitle: '',
         description: '  These data sets contain nuclear observations which are made using some speciality spectrometers that detect interactions of highly energetic particles.  These can be an X-ray spectrometer that measures X-rays that are emitted from the surface.  Another one is a neutron spectrometer, which detects different energy states of neutrons that are created when cosmic rays impact the surface.  Detailed analysis can separate particles, atoms, and molecules by their mass, momentum, or energy. Data from these observations can be used to determine chemical composition and abundance.  ',
-        url: '/pds/archive/xgn.html',
+        url: '/pds-staging/archive/xgn.html',
         category: 'Data Types'
     },{
         name: 'Orbital data',
         subtitle: 'proper elements and dynamical families',
         description: ' These data sets contain derived data relating to the orbits of objects. These data sets will include the proper elements and dynamical families. Orbital observations can be ground based or from spacecraft. Orbital data sets are typically collected over a longer time period to allow for a complete observation of an orbital path.  ',
-        url: '/pds/archive/orbital.html',
+        url: '/pds-staging/archive/orbital.html',
         category: 'Data Types'
     },{
         name: 'Photometry and Lightcurves',
         subtitle: '',
         description: ' These data sets contain Photometry observations, which are used to measure the brightness of celestial objects. These measurements can provide information about an object’s size, distance, temperature, and rotational period. These data sets are mostly asteroid data sets with some comets and other solar system bodies.',
-        url: '/pds/archive/phot.html',
+        url: '/pds-staging/archive/phot.html',
         category: 'Data Types'
     },{
         name: 'Physical Properties',
         subtitle: 'spin vectors, taxonomic classification, albedos, densities, and diameters',
         description: 'These data sets include observations on the physical properties of asteroids such as spin vectors, taxonomic classifications, albedos, densities and diameters.',
-        url: '/pds/archive/physical.html',
+        url: '/pds-staging/archive/physical.html',
         category: 'Data Types'
     },{
         name: 'Polarimetry',
         subtitle: '',
         description: 'These datasets contain the measurement of polarization of electromagnetic waves such as radio or light waves (the orientation of the light waves). Typically polarimetry is done on electromagnetic waves that have traveled through or have been reflected, refracted or diffracted by some material in order to characterize that object. ',
-        url: '/pds/archive/polarimetry.html',
+        url: '/pds-staging/archive/polarimetry.html',
         category: 'Data Types'
     },{
         name: 'Radar',
         subtitle: '',
         description: ' These data sets contain radar observations which is a measurement system that uses radio waves (Radar) to determine the range, angle, or velocity of objects. This is done by reflecting radar waves off of a target and analyzing the reflections. Radar data can be used to construct both radar images and shape models of objects.  ',
-        url: '/pds/archive/radar.html',
+        url: '/pds-staging/archive/radar.html',
         category: 'Data Types'
     },{
         name: 'Radio Science',
         subtitle: '',
         description: ' These data sets contain Radio science data, which makes use of the radio telemetry signal between the spacecraft and radio telescopes on Earth to accurately measure position of the spacecraft.  These measurements can be used to determine the gravity field, mass, and other physical characteristics of a body the spacecraft encounters. ',
-        url: '/pds/archive/rs.html',
+        url: '/pds-staging/archive/rs.html',
         category: 'Data Types'
     },{
         name: 'Shape Models',
         subtitle: '',
         description: 'These data sets contain Shape models (or Digital Terrain Models) three-dimensional models of the shape of an object. They can be derived using radar data, visible image data, light curves with inversion techniques and/or laser altimeter data. ',
-        url: '/pds/shape-models/',
+        url: '/pds-staging/shape-models/',
         category: 'Data Types'
     },{
         name: 'Gravity Models',
         subtitle: '',
         description: ' These data sets contain gravity models which are a detailed model of the gravitational field of an object, such as spherical harmonics, radial gravity acceleration, or internal density.  Gravity models are derived from radio science data which make use of the radio telemetry signal between the spacecraft and earth to accurately determine the position of the spacecraft ',
-        url: '/pds/archive/gravity.html',
+        url: '/pds-staging/archive/gravity.html',
         category: 'Data Types'
     },{
         name: 'SPICE Kernels',
         subtitle: '',
         description: ' These data sets include SPICE data, which contains time-series details of the position and pointing of the spacecraft, and the position and rotation state of the planets/small bodies.  It also includes exact definitions of the spacecraft equipment and instruments, to include how they are oriented on the spacecraft. The complete SPICE information provides deterministic information to compute relative and inertial positions, pointing and angles of objects in space.',
-        url: '/pds/archive/spice.html',
+        url: '/pds-staging/archive/spice.html',
         category: 'Data Types'
     },{
         name: 'Spectrophotometry and Spectra',
         subtitle: '',
         description: ` These data sets contain Spectrophotometry observations which are the quantitative measurement of the reflection or transmission properties of a material as a function of wavelength.This data can also be commonly represented as a chart or a graph that shows the intensity of light being emitted over a range of wavelengths (or wavenumbers). `,
-        url: '/pds/archive/spectra.html',
+        url: '/pds-staging/archive/spectra.html',
         category: 'Data Types'
     },{
         name: 'All Asteroid Data',
         subtitle: '',
-        url: '/pds/archive/asteroids.html',
+        url: '/pds-staging/archive/asteroids.html',
         category: 'Target Types'
     },{
         name: 'All Dust Data',
         subtitle: '',
-        url: '/pds/archive/dust.html',
+        url: '/pds-staging/archive/dust.html',
         category: 'Target Types'
     },{
         name: 'Near Earth Objects',
         subtitle: '',
-        url: '/pds/archive/neos.html',
+        url: '/pds-staging/archive/neos.html',
         category: 'Target Types'
     },{
         name: 'TNOs, Centaurs, and Pluto',
         subtitle: '',
-        url: '/pds/archive/tnos.html',
+        url: '/pds-staging/archive/tnos.html',
         category: 'Target Types'
     },{
         name: 'Planetary Satellites',
         subtitle: '',
-        url: '/pds/archive/sat.html',
+        url: '/pds-staging/archive/sat.html',
         category: 'Target Types'
     },{
         name: 'Meteorites',
         subtitle: '',
-        url: '/pds/archive/meteorite.html',
+        url: '/pds-staging/archive/meteorite.html',
         category: 'Target Types'
     },{
         name: 'Meteoroids',
         subtitle: '',
-        url: '/pds/archive/meteoroid.html',
+        url: '/pds-staging/archive/meteoroid.html',
         category: 'Target Types'
     },{
         name: 'Comets',
         subtitle: '',
-        url: '/pds/archive/comets.html',
+        url: '/pds-staging/archive/comets.html',
         category: 'Target Types'
     },{
         name: 'Laboratory Spectra',
         subtitle: '',
-        url: '/pds/archive/lab.html',
+        url: '/pds-staging/archive/lab.html',
         category: 'Target Types'
     },{
         name: 'Bopps',
@@ -155,13 +155,13 @@ searchData.splice(0, 0, {
         name: 'Cassini',
         subtitle: 'Interplanetary dust and Small satellite shape models',
         instruments: ['HRD', 'CDA',],
-        url: '/pds/archive/cassini.html',
+        url: '/pds-staging/archive/cassini.html',
         category: 'Mission'
     },{
         name: 'Dawn',
         subtitle: '4 Vesta, 1 Ceres',
         instruments: ['VIR', 'FC', 'GRaND',],
-        url: '/pds/resource/dawn',
+        url: '/pds-staging/resource/dawn',
         category: 'Mission'
     },{
         name: 'Deep Impact',
@@ -185,7 +185,7 @@ searchData.splice(0, 0, {
         name: 'Galileo',
         subtitle: '243 Ida, 951 Gaspra, Small planetary satellites, Interplanetary dust',
         instruments: ['Galileo dust detector system (GDDS)',],
-        url: '/pds/archive/galileo.html',
+        url: '/pds-staging/archive/galileo.html',
         category: 'Mission'
     },{
         name: 'Giotto',
@@ -197,61 +197,61 @@ searchData.splice(0, 0, {
         name: 'Hayabusa',
         subtitle: '25143 Itokawa',
         instruments: ['Asteroid Multi-band Imaging Camera (AMICA)', 'Near-Infrared Spectrometer (NIRS)', 'X-Ray Spectrometer (XRS)', 'Light Detection and Ranging (LIDAR)',],
-        url: '/pds/archive/hayabusa.html',
+        url: '/pds-staging/archive/hayabusa.html',
         category: 'Mission'
     },{
         name: 'Hayabusa2',
         subtitle: '162173 Ryugu',
         instruments: ['Optical Navigation Cameras (ONC)', 'Thermal Infrared Imager (TIR)', 'Near Infrared Spectrometer (NIRS3)', 'Light Detection and Ranging (LIDAR)', 'MASCOT lander', 'Minerva landers', 'Deployable Camera (DCAM3)',],
-        url: '/pds/resource/hayabusa2',
+        url: '/pds-staging/resource/hayabusa2',
         category: 'Mission'
     },{
         name: 'Hubble Space Telescope',
         subtitle: '1 Ceres, 4 Vesta',
         instruments: ['Hubble',],
-        url: '/pds/archive/sc.html',
+        url: '/pds-staging/archive/sc.html',
         category: 'Mission'
     },{
         name: 'IRAS',
         subtitle: 'Interplanetary dust, Asteroids',
         instruments: ['Focal Plane Array (FPA)',],
-        url: '/pds/archive/iras.html',
+        url: '/pds-staging/archive/iras.html',
         category: 'Mission'
     },{
         name: 'LADEE',
         subtitle: 'Lunar dust',
         instruments: ['Lunar Dust Experiment (LDEX)', 'Neutral Mass Spectrometer (NMS)', 'UV-Vis Spectrometer (UVS)'],
-        url: '/pds/archive/ladee.html',
+        url: '/pds-staging/archive/ladee.html',
         category: 'Mission'
     },{
         name: 'Mariner 9',
         subtitle: 'Deimos',
         instruments: [''],
-        url: '/pds/archive/sc.html',
+        url: '/pds-staging/archive/sc.html',
         category: 'Mission'
     },{
         name: 'MSX',
         subtitle: 'Interplanetary dust, Asteroids, Comets',
         instruments: ['Spirit III',],
-        url: '/pds/archive/msx.html',
+        url: '/pds-staging/archive/msx.html',
         category: 'Mission'
     },{
         name: 'NEAR',
         subtitle: '433 Eros, 253 Mathilde, C/Hyakutake (1996 B2)',
         instruments: ['Multi-Spectral Imager (MSI)', 'Near-Infrared Spectrometer (NIRS)', 'Gamma-Ray Spectrometer (GRS)', 'X-ray Spectromter (XRS)', 'NEAR Magnetometer (MAG)', 'NEAR Laser Rangefinder (NLR)', 'Radio Science Subsystem (RSS)',],
-        url: '/pds/resource/near',
+        url: '/pds-staging/resource/near',
         category: 'Mission'
     },{
         name: 'NEO Surveys',
         subtitle: 'Asteroids and Comets',
         instruments: [],
-        url: '/pds/archive/surveys.html',
+        url: '/pds-staging/archive/surveys.html',
         category: 'Mission'
     },{
         name: 'NEOWISE',
         subtitle: 'Asteroids, Satellites of Saturn',
         instruments: [],
-        url: '/pds/resource/neowisediam.html',
+        url: '/pds-staging/resource/neowisediam.html',
         category: 'Mission'
     },{
         name: 'New Horizons',
@@ -269,7 +269,7 @@ searchData.splice(0, 0, {
         name: 'OSIRIS-REx',
         subtitle: '101955 Bennu',
         instruments: ['OCAMS', 'TAGCAMS', 'OVIRS', 'OTES', 'REXIS', 'OLA', 'Deep Space Network (DSN)',],
-        url: '/pds/resource/orex/',
+        url: '/pds-staging/resource/orex/',
         category: 'Mission'
     },{
         name: 'Rosetta',
@@ -288,24 +288,24 @@ searchData.splice(0, 0, {
         name: 'Ulysses',
         subtitle: 'Interplanetary dust',
         instruments: ['Ulysses dust detector system (UDDS)',],
-        url: '/pds/resource/udds.html',
+        url: '/pds-staging/resource/udds.html',
         category: 'Mission'
     },{
         name: 'Vega 1 and 2',
         subtitle: '1P/Halley (1682 Q1)',
         instruments: [''],
-        url: '/pds/archive/sc.html',
+        url: '/pds-staging/archive/sc.html',
         category: 'Mission'
     },{
         name: 'Viking Orbiter',
         subtitle: 'Phobos, Deimos',
         instruments: [''],
-        url: '/pds/archive/sc.html',
+        url: '/pds-staging/archive/sc.html',
         category: 'Mission'
     },{
         name: 'Voyager 1 and 2',
         subtitle: 'Small planetary satellites',
-        url: '/pds/archive/sc.html',
+        url: '/pds-staging/archive/sc.html',
         category: 'Mission'
     }
 );
